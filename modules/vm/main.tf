be8b4a52756d2cfec6c59ac1b6b3a0c46d989a10
JunoInk <39061197+JunoInk@users.noreply.github.com>
2026-03-29 13:35:36 -0400

    Create main.tf
    For Azure Linux VM

---
+++ modules/vm/main.tf
@@ -0,0 +1,26 @@
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "${var.environment}-vm"
+  resource_group_name = var.resource_group
+  location            = var.location
+  size                = "Standard_B2s"
+  admin_username      = "azureuser"
+
+  network_interface_ids = [var.nic_id]
+
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+}
